afeebd16ef742fe2efbf09e316caed21e9421329
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-01-24 21:16:10 +0100

    style up the Joke component, add necessary classes, and a smiley next to the joke text

--- src/Joke.js
+++ src/Joke.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import './Joke.css';
 
 const MAX_VOTE_NUM = 10;
 const MIN_VOTE_NUM = -10;
@@ -10,8 +11,8 @@ class Joke extends Component {
     let downvoteDisabled = this.props.votes === MIN_VOTE_NUM;
     let upvoteDisabled = this.props.votes === MAX_VOTE_NUM;
     return (
-      <div>
-        <div>
+      <div className='Joke'>
+        <div className='Joke-voteline'>
           <button onClick={this.props.upvote} disabled={upvoteDisabled}>
             <FontAwesomeIcon icon={faArrowUp} />
           </button>
@@ -20,7 +21,14 @@ class Joke extends Component {
             <FontAwesomeIcon icon={faArrowDown} />
           </button>
         </div>
-        <div>{this.props.text}</div>
+        <div className='Joke-text'>{this.props.text}</div>
+        <div className='Joke-smiley'>
+          <i
+            class='em em-laughing'
+            aria-role='presentation'
+            aria-label='SMILING FACE WITH OPEN MOUTH AND TIGHTLY-CLOSED EYES'
+          ></i>
+        </div>
       </div>
     );
   }
