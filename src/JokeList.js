bde61ed746c3c4956ca86b751b1a2372a4514cbe
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-01-26 20:41:41 +0100

    add alt prop to the JokeList-sidebar emoji img

--- src/JokeList.js
+++ src/JokeList.js
@@ -85,6 +85,11 @@ class JokeList extends Component {
           </h1>
           <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
           <button onClick={this.handleLoadMoreJokes}>New Jokes</button>
+          <img
+            src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg'
+            alt='JokeList-sidebar-laughing-emoji'
+          />
+          <button>New Jokes</button>
         </div>
         <div className='JokeList-jokes'>{this.renderedJokes()}</div>
       </div>
